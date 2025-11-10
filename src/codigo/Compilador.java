e471ddf9da461807ad4acf91f8945053885324b7
Alan Cuevas <alcuevasal@ittepic.edu.mx>
2025-11-10 16:47:57 -0700

    se implementaron el guardar, nuevo y abrir archivos y la parte del analisis léxico

--- src/codigo/Compilador.java
+++ src/codigo/Compilador.java
@@ -1,15 +1,29 @@
 
 package codigo;
 
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.io.*;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
 
 
 public class Compilador extends javax.swing.JFrame {
+    // Variable global para saber si el archivo actual ya está guardado
+    private java.io.File archivoActual = null;
+    private boolean cambiosSinGuardar = false;
+    private boolean actualizandoTexto = false;
+    public String rute="";
 
 
     /**
@@ -41,10 +55,183 @@ public class Compilador extends javax.swing.JFrame {
             public void changedUpdate(DocumentEvent e) {
                numerolinea.repaint();
             }
-        
+            
         });
     }
     
+    
+    public void GuardarArchivo(){
+        javax.swing.JFileChooser fileChooser = new javax.swing.JFileChooser();
+        fileChooser.setDialogTitle("Guardar archivo .quim");
+
+        // Filtro para archivos .quim
+        javax.swing.filechooser.FileNameExtensionFilter filter =
+        new javax.swing.filechooser.FileNameExtensionFilter("Archivos Quim (*.quim)", "quim");
+        fileChooser.setFileFilter(filter);
+
+        int seleccion = fileChooser.showSaveDialog(this);
+
+        if (seleccion == javax.swing.JFileChooser.APPROVE_OPTION) {
+            java.io.File archivo = fileChooser.getSelectedFile();
+        // Si el nombre no termina con .quim, se lo agregamos automáticamente
+            if (!archivo.getName().toLowerCase().endsWith(".quim")) {
+                archivo = new java.io.File(archivo.getAbsolutePath() + ".quim");
+            }
+
+            try (java.io.BufferedWriter writer = new java.io.BufferedWriter(new java.io.FileWriter(archivo))) {
+                //Obtener el contenido del jCode y escribirlo
+                writer.write(jCode.getText());
+                writer.flush();
+                
+                javax.swing.JOptionPane.showMessageDialog(this,
+                    "Archivo guardado correctamente en:\n" + archivo.getAbsolutePath(),
+                "Guardar archivo",
+                javax.swing.JOptionPane.INFORMATION_MESSAGE);
+
+            } catch (Exception e) {
+                javax.swing.JOptionPane.showMessageDialog(this,
+                "Error al guardar el archivo: " + e.getMessage(),
+                "Error",
+                javax.swing.JOptionPane.ERROR_MESSAGE);
+            }
+       } 
+    }
+    
+    public void NuevoArchivo(){
+        if (jCode.getText().trim().isEmpty()) {
+            jCode.setText("");
+            archivoActual = null;
+            cambiosSinGuardar = false;
+            return;
+        }
+
+        //Si hay cambios sin guardar, preguntar al usuario
+        if (cambiosSinGuardar) {
+            int opcion = javax.swing.JOptionPane.showConfirmDialog(this,
+              "Tienes cambios sin guardar.\n¿Deseas guardarlos antes de crear un nuevo archivo?", "Guardar cambios",
+            javax.swing.JOptionPane.YES_NO_CANCEL_OPTION, javax.swing.JOptionPane.WARNING_MESSAGE);
+            
+            if (opcion == javax.swing.JOptionPane.CANCEL_OPTION) {
+                return; // cancelar acción
+            } else if (opcion == javax.swing.JOptionPane.YES_OPTION) {
+                GuardarArchivo(); // Guardamos antes de limpiar
+            }
+        }
+        // Limpiar el área y reiniciar el estado
+        jCode.setText("");
+        archivoActual = null;
+        cambiosSinGuardar = false;   
+    }
+    
+    public void AbrirArchivo(){
+        javax.swing.JFileChooser fileChooser = new javax.swing.JFileChooser();
+        fileChooser.setDialogTitle("Abrir archivo .quim");
+
+        // 🔹 Solo archivos con extensión .quim
+        javax.swing.filechooser.FileNameExtensionFilter filter = 
+                new javax.swing.filechooser.FileNameExtensionFilter("Archivos Quim (*.quim)", "quim");
+        fileChooser.setFileFilter(filter);
+
+        int seleccion = fileChooser.showOpenDialog(this);
+
+        if (seleccion == javax.swing.JFileChooser.APPROVE_OPTION) {
+                java.io.File archivo = fileChooser.getSelectedFile();
+
+            // Validar la extensión manualmente
+                if (!archivo.getName().toLowerCase().endsWith(".quim")) {
+                    javax.swing.JOptionPane.showMessageDialog(this,
+
+                        "El archivo seleccionado no es compatible.\nDebe ser un archivo con extensión .quim",
+                        "Archivo no compatible",
+                    javax.swing.JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+
+                try (java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.FileReader(archivo))) {
+                    StringBuilder contenido = new StringBuilder();
+                    String linea;
+
+                    while ((linea = reader.readLine()) != null) {
+                        contenido.append(linea).append("\n");
+                    }
+
+                    // Mostrar el contenido del archivo .quim en el editor
+                    jCode.setText(contenido.toString());
+
+                    javax.swing.JOptionPane.showMessageDialog(this,
+                        "Archivo .quim cargado correctamente:\n" + archivo.getAbsolutePath(),
+                     "Abrir archivo", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+                    rute = archivo.getAbsolutePath();
+
+                } catch (Exception e) {
+                    javax.swing.JOptionPane.showMessageDialog(this, "Error al abrir el archivo: " + e.getMessage(),
+                            "Error", javax.swing.JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+    
+    private void ejecutarAnalisisLexico(){
+        try {
+            String codigo = jCode.getText();
+            Reader lector = new BufferedReader(new StringReader(codigo));
+            Lexer lexer = new Lexer(lector);
+            
+            if (codigo == null || codigo.trim().isEmpty()){
+                JOptionPane.showMessageDialog(this,
+                        "No hay codigo para analizar. \nPor favor escribe o carga un programa antes de ejecutar el análisis léxico.",
+                        "Sin contenido",
+                        JOptionPane.WARNING_MESSAGE
+                );
+                return;
+            }
+
+            DefaultTableModel tokens = new DefaultTableModel();
+            tokens.addColumn("#");
+            tokens.addColumn("Lexema");
+            tokens.addColumn("Comp. Léxico");
+
+            StringBuilder errores = new StringBuilder();
+
+            Tokens token;
+            while((token = lexer.yylex()) != null){
+                int linea = token.getLinea();
+                String lexema = token.getLexema();
+                String compLexico = token.getComponente();
+
+                //Si el token pertenece a algún tipo de error, lo mandamos a jErrores
+                if(token.toString().startsWith("ERROR")){
+                    errores.append(String.format("Linea %d -> %s (%s)&%n",linea, lexema, token));
+                } else {
+                    tokens.addRow(new Object[]{ linea, lexema, compLexico});
+                }
+            }
+
+            // Mostrar los resultados en una ventana aparte
+            JTable tablaTokens = new JTable(tokens);
+            JScrollPane scroll = new JScrollPane(tablaTokens);
+
+            JDialog ventana = new JDialog(this, "Tabla de Tokens", true);
+            ventana.setSize(400, 600);
+            ventana.setLocationRelativeTo(this);
+            ventana.setLayout(new BorderLayout(10, 10));
+            ventana.add(scroll, BorderLayout.CENTER);
+
+            ventana.setVisible(true);
+
+            if (errores.length() == 0) {
+                jErrores.setText("Análisis léxico completado sin errores.");
+            } else {
+                jErrores.setText(" Se encontraron errores léxicos:\n\n" + errores.toString());
+            }
+            
+        } catch (Exception e) {
+             jErrores.setText("️ Error en el análisis léxico: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+        
+        
+    }
     
 
     /**
@@ -60,7 +247,7 @@ public class Compilador extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jCode = new javax.swing.JTextPane();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextPane1 = new javax.swing.JTextPane();
+        jErrores = new javax.swing.JTextPane();
         jLabel1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         menuArchivo = new javax.swing.JMenu();
@@ -83,9 +270,9 @@ public class Compilador extends javax.swing.JFrame {
         jCode.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
         jScrollPane1.setViewportView(jCode);
 
-        jTextPane1.setBorder(null);
-        jTextPane1.setRequestFocusEnabled(false);
-        jScrollPane2.setViewportView(jTextPane1);
+        jErrores.setBorder(null);
+        jErrores.setRequestFocusEnabled(false);
+        jScrollPane2.setViewportView(jErrores);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -125,6 +312,11 @@ public class Compilador extends javax.swing.JFrame {
 
         jMenuItem7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jMenuItem7.setText("Nuevo");
+        jMenuItem7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem7ActionPerformed(evt);
+            }
+        });
         menuArchivo.add(jMenuItem7);
 
         jMenuItem1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
@@ -153,6 +345,11 @@ public class Compilador extends javax.swing.JFrame {
 
         jMenuItem3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jMenuItem3.setText("Analisis léxico");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         menuCompilar.add(jMenuItem3);
 
         jMenuBar1.add(menuCompilar);
@@ -202,11 +399,11 @@ public class Compilador extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
+        GuardarArchivo();
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        // TODO add your handling code here:
+        AbrirArchivo();
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
@@ -216,6 +413,14 @@ public class Compilador extends javax.swing.JFrame {
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
       
     }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
+        NuevoArchivo();
+    }//GEN-LAST:event_jMenuItem7ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+       ejecutarAnalisisLexico();
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -255,6 +460,7 @@ public class Compilador extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextPane jCode;
+    private javax.swing.JTextPane jErrores;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
@@ -267,7 +473,6 @@ public class Compilador extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextPane jTextPane1;
     private javax.swing.JMenu menuArchivo;
     private javax.swing.JMenu menuCompilar;
     private javax.swing.JMenu menuTablaSimbolos;
